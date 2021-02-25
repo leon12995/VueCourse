dec64234fa83bab35f3db57af99aaa2ab9da0881
leon12995@gmail.com <leon12995@gmail.com>
2021-02-26 00:32:32 +0800

    day 19

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,13 +1,22 @@
 <template>
-    <button @click="addOne">Add 2</button>
+    <div>
+        <button @click="inc">Add 2</button>
+        <button @click="increase({value: 11})">Add 11</button>
+    </div>
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
     methods: {
-        addOne(){
-            this.$store.dispatch('increment');
-        }
+        // addOne(){
+        //     this.$store.dispatch('increment');
+        // }
+        // ...mapActions(['increment','increase'])
+        ...mapActions({
+            inc: 'numbers/increment',
+            increase: 'numbers/increase'
+        })
     }
 }
 </script>
